8e5306d7237def6bc02c03482d0d94d05f317bdc
Joel <98235957+convurt@users.noreply.github.com>
2022-11-10 09:59:21 -0500

    Changing to hooks instead of classes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
@@ -7,51 +7,43 @@ import ErrorBoundry from "../components/ErrorBoundry";
 // STATE >> props
 
 //the class app controls the application
-class App extends Component {
-    //create the state (smart component), super() is a reference to the parent class and has to come first
-    // look at life cycle hooks https://reactjs.org/docs/react-component.html
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: ""
-        }
-        console.log("constructor");
-    }
+function App() {
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState("")
 
-    componentDidMount() {
+    useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then(response => response.json())
-            .then(users => this.setState({robots: users}));
-    }
+            .then(users => {
+                setRobots(users)
+            });
+    },[])
 
     //changing the state when searching in searchbox
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
+    }
 
-    }
-    render() {
-        // structure robts and searchfield so you dont have to use this.state.robots and this.state.searchfield
-        const { robots, searchfield } = this.state;
-        //filtering the robots with the searchfield from above, the includes is what connected them together
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-            return !robots.length ?
-            <h1>Loading</h1> :
-            (
-                <div className="tc">
-                    <h1>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList robots={filteredRobots}/>
-                        </ErrorBoundry>
-                    </Scroll>
-                </div>
-            );
-        }
-    }
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+
+    return !robots.length ?
+        <h1>Loading</h1> :
+        (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots}/>
+                    </ErrorBoundry>
+                </Scroll>
+            </div>
+        );
+
+}
+
 
 
 export default App;
